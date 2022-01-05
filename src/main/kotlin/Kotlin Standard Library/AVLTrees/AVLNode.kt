d34e040eeb1840d9574d0ec1835b596161d2fa1f
reptilefury <reptilefury@outlook.com>
2022-01-05 06:26:11 +0300

    Started AVL trees

---
+++ src/main/kotlin/Kotlin Standard Library/AVLTrees/AVLNode.kt
@@ -0,0 +1,47 @@
+package `Kotlin Standard Library`.AVLTrees
+
+typealias Visitor<T> = (T) -> Unit
+
+class AVLNode<T>(var value: T) {
+    var leftChild: AVLNode<T>? = null
+    var rightChild: AVLNode<T>? = null
+
+    val min: AVLNode<T>
+        get() = min?.leftChild ?: this
+
+    override fun toString() = diagram(this)
+
+    private fun diagram(
+        node: AVLNode<T>?,
+        top: String = "",
+        root: String = "",
+        bottom: String = ""
+    ): String {
+        return node?.let {
+            if (node.leftChild == null && node.rightChild == null) {
+                "$root${node.value}\n"
+            } else {
+                diagram(node.rightChild, "$top ", "$top┌──", "$top│ ") +
+                        root + "${node.value}\n" + diagram(node.leftChild, "$bottom│ ", "$bottom└──", "$bottom ")
+            }
+        } ?: "${root}null\n"
+    }
+
+    fun traverseInOrder(visit: Visitor<T>) {
+        leftChild?.traverseInOrder(visit)
+        visit(value)
+        rightChild?.traverseInOrder(visit)
+    }
+
+    fun traversePreOrder(visit: Visitor<T>) {
+        visit(value)
+        leftChild?.traversePreOrder(visit)
+        rightChild?.traversePreOrder(visit)
+    }
+
+    fun traversePostOrder(visit: Visitor<T>) {
+        leftChild?.traversePostOrder(visit)
+        rightChild?.traversePostOrder(visit)
+        visit(value)
+    }
+}
---
+++ src/main/kotlin/Kotlin Standard Library/AVLTrees/AVLTree.kt
@@ -0,0 +1,69 @@
+package `Kotlin Standard Library`.AVLTrees
+
+class AVLTree<T : Comparable<T>> {
+    var root: AVLNode<T>? = null
+    fun insert(value: T) {
+        root = insert(root, value)
+    }
+
+    private fun insert(node: AVLNode<T>?, value: T): AVLNode<T> {
+        node ?: return AVLNode(value)
+
+        if (value <= node.value) {
+            node.leftChild = insert(node.leftChild, value)
+        } else {
+            node.rightChild = insert(node.rightChild, value)
+        }
+        return node
+    }
+
+    fun remove(value: T) {
+        root = remove(root!!, value)
+    }
+
+    private fun remove(node: AVLNode<T>, value: T): AVLNode<T>? {
+        node ?: return null
+        when {
+            value == node.value -> {
+
+                if (node.leftChild == null && node.rightChild == null) {
+                    return null
+                }
+
+                if (node.leftChild == null) {
+                    return node.rightChild
+                }
+                if (node.rightChild == null) {
+                    return node.leftChild
+                }
+
+                node.rightChild?.min?.value.let {
+                    node.value = it!!
+                }
+                node.rightChild = remove(node.rightChild!!, node.value)
+            }
+            value < node.value -> node.leftChild = remove(node.leftChild!!, value)
+            else -> node.rightChild = remove(node.rightChild!!, value)
+        }
+        return node
+    }
+
+    override fun toString() = root?.toString() ?: " empty tree"
+    fun contains(value: T): Boolean {
+        var current = root
+
+
+        while (current != null) {
+            if (current.value == value) {
+                return true
+            }
+
+            current = if (value < current.value) {
+                current.leftChild
+            } else {
+                current.rightChild
+            }
+        }
+        return false
+    }
+}
