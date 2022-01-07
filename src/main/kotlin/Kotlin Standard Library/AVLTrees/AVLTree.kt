09ab0d6ea1ac26df6b9380afba8e17617c920f78
reptilefury <reptilefury@outlook.com>
2022-01-07 05:54:27 +0300

    Rotation of an AVL

--- src/main/kotlin/Kotlin Standard Library/AVLTrees/AVLTree.kt
+++ src/main/kotlin/Kotlin Standard Library/AVLTrees/AVLTree.kt
@@ -66,6 +66,7 @@ class AVLTree<T : Comparable<T>> {
         }
         return false
     }
+
     private fun leftRotate(node: AVLNode<T>): AVLNode<T> {
         val pivot = node.rightChild!!
         node.rightChild = pivot.leftChild
@@ -74,12 +75,47 @@ class AVLTree<T : Comparable<T>> {
         pivot.height = max(pivot.rightHeight, pivot.leftHeight) + 1
         return pivot
     }
-    private fun rightRotate(node: AVLNode<T>):AVLNode<T>{
-        val  pivot = node.leftChild!!
+
+    private fun rightRotate(node: AVLNode<T>): AVLNode<T> {
+        val pivot = node.leftChild!!
         node.leftChild = pivot.rightChild
         pivot.rightChild = node
-        node.height = max(node.leftHeight, node.rightHeight) +1
+        node.height = max(node.leftHeight, node.rightHeight) + 1
         pivot.height = max(pivot.leftHeight, pivot.rightHeight) + 1
-        return  node
+        return node
     }
+
+    private fun rightLeftRotate(node: AVLNode<T>): AVLNode<T> {
+        val rightChild = node.rightChild ?: node
+        node.rightChild = rightRotate(rightChild)
+        return leftRotate(node)
+    }
+
+    private fun leftRightRotate(node: AVLNode<T>): AVLNode<T> {
+        val leftChild = node.leftChild ?: node
+        node.leftChild = rightRotate(leftChild)
+        return rightRotate(node)
+    }
+
+    private fun balance(node: AVLNode<T>): AVLNode<T> {
+        return when (node.balanceFactor) {
+            2 -> {
+                if (node.leftChild?.balanceFactor == -1) {
+                    leftRightRotate(node)
+                } else {
+                    rightRotate(node)
+                }
+            }
+            -2 -> {
+                 if(node.rightChild?.balanceFactor == 1){
+                     rightLeftRotate(node)
+                 } else {
+                     leftRotate(node)
+                 }
+            }
+            else -> node
+        }
+    }
+
+
 }
