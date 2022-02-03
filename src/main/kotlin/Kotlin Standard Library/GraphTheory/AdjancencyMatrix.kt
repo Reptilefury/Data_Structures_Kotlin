1c5978b131faf0f2bf855911227144d44ecb9a39
reptilefury <reptilefury@outlook.com>
2022-02-03 07:06:36 +0300

    Adjancency list

--- src/main/kotlin/Kotlin Standard Library/GraphTheory/AdjancencyMatrix.kt
+++ src/main/kotlin/Kotlin Standard Library/GraphTheory/AdjancencyMatrix.kt
@@ -1,10 +1,12 @@
 package `Kotlin Standard Library`.GraphTheory
 
+import `Kotlin Standard Library`.size
+
 class AdjancencyMatrix<T> : Graph<T> {
     val vertices = ArrayList<Vertex<T>>()
     val weights = ArrayList<Double?>()
     override fun weight(source: Vertex<T>, destination: Vertex<T>): Double? {
-
+        return  weights[source.index][destination.index]
     }
 
     override fun createVertex(data: T): Vertex<T> {
@@ -29,7 +31,15 @@ class AdjancencyMatrix<T> : Graph<T> {
         weights.add(row)
         return vertex
     }
-    override fun edges(source: Vertex<T>, destination: Vertex<T>, weight: Double?): ArrayList<Edge<T>> {
-         weights[source.index][destination.index] = weight
+    override fun edges(source: Vertex<T>): ArrayList<Edge<T>> {
+        val edges = arrayListOf<Edge<T>>()
+        (0 until weights.size).forEach {
+            column ->
+            val weight = weights[source.index][column]
+            if(weight != null){
+                edges.add(Edge(source, vertices[column], weight))
+            }
+        }
+    return  edges
     }
 }
